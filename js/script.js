b2edd43a6906bc1decd18a346f2f7b862d6dfa3a
precoder365 <prenitharajesh04@gmail.com>
2024-01-27 16:59:40 +0530

    Add ScrollReveal library for animations

--- js/script.js
+++ js/script.js
@@ -2,3 +2,13 @@ const header = document.querySelector("header");
 window.addEventListener("scroll", () => {
   header.classList.toggle("scrolled", window.scrollY > 0);
 });
+
+// Scroll reveal
+
+let sr = ScrollReveal({
+  distance: "60px",
+  duration: 2500,
+});
+
+sr.reveal(".showcase-info",{delay:600});
+sr.reveal(".showcase-image",{origin:"top",delay:700});
