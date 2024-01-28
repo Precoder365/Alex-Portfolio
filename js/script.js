bf26383378e56466a5c9cdf5e87db7e382f4edf0
precoder365 <prenitharajesh04@gmail.com>
2024-01-28 16:54:46 +0530

    Add services section with milestones and counter

--- js/script.js
+++ js/script.js
@@ -1,6 +1,11 @@
 const header = document.querySelector("header");
+
+const ml_section=document.querySelector(".milestones");
+const ml_counters=document.querySelectorAll(".number span");
+
 window.addEventListener("scroll", () => {
   header.classList.toggle("scrolled", window.scrollY > 0);
+  if(!mlPlayed) mlCounter();
 });
 
 // Scroll reveal
@@ -12,3 +17,37 @@ let sr = ScrollReveal({
 
 sr.reveal(".showcase-info",{delay:100});
 sr.reveal(".showcase-image",{origin:"top",delay:200});
+
+function hasReached(element) {
+  const elementTop = element.getBoundingClientRect().top;
+  if (window.innerHeight >= elementTop + element.offsetHeight) {
+    return true;
+  }
+  return false;
+}
+
+// Counter
+
+function updateCount(num, maxNum){
+  let currentNum=+num.innerText;
+  if(currentNum<maxNum){
+    num.innerText=currentNum+1;
+    setTimeout(()=>{
+      updateCount(num,maxNum);
+    },25);
+  }
+}
+
+let mlPlayed=false;
+
+function mlCounter() {
+  if(!hasReached(ml_section)) return;
+  mlPlayed=true;
+  ml_counters.forEach(ctr=>{
+    let target=+ctr.dataset.target;
+    console.log(target);
+    setTimeout(()=>{
+      updateCount(ctr,target);
+    },400);
+  });
+}
