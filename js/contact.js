9bdd6c179c71289a2549aa56171e9cc217129a82
precoder365 <prenitharajesh04@gmail.com>
2024-01-31 18:22:38 +0530

    Add external js form validation

--- js/contact.js
+++ js/contact.js
@@ -21,3 +21,27 @@ function hasReached(element) {
   }
   return false;
 }
+
+// Form validation
+
+const form = document.querySelector("form");
+if(form){
+  const name = document.querySelector("#name");
+  const email = document.querySelector("#email");
+  const message = document.querySelector("#message");
+
+  
+  form.addEventListener("submit", (e) => {
+    e.preventDefault();
+    if (name.value==="" || email.value === "" || message.value === "") {
+      alert("Please enter all fields");
+    } if(name.value.length < 3){
+      alert("Name must be at least 3 characters long");
+    }
+    else {
+      alert("Message sent!");
+      email.value = "";
+      message.value = "";
+    }
+  });
+}
